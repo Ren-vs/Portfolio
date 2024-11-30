df0fb548897a9a10db7b3db542834c555060fecb
Ren-vs <dietyaphrodite8@gmail.com>
2024-11-30 22:00:52 +0800

    Add files via upload
    all files

---
+++ script.js
@@ -0,0 +1,8 @@
+/*=============================Additionally this java script is used as text switcher between the text's i listed below.====================*/
+
+var typed = new Typed(".typing", {
+     strings:["A CompSci Student", "Studying in Partido State University"],
+     typeSpeed:80,
+     backSpeed:80,
+     loop:true
+})
